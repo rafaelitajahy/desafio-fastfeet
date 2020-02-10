d7a2f7a49ae6fb44966559d5d2d5d5d2639a0fdf
Rafael Carvalho Itajahy <rafaelitajahy@gmail.com>
2020-02-10 20:01:54 -0300

    ADD: Avatar do usuário

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,12 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
 import Recipient from '../app/models/Recipient';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Recipient];
+const models = [User, File, Recipient];
 
 class Database {
   constructor() {
@@ -15,7 +16,9 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseConfig);
 
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 import RecipientController from './app/controllers/RecipientController';
 
 import authMiddlewar from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -14,6 +18,9 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddlewar);
 
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
 
